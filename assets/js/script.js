015e204ba964c1d438dfaf4c3492d99c471472da
joaovictor <joaovictorlira18@hotmail.com>
2024-11-30 22:38:59 -0300

    adding new functions and styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@
 const button = document.querySelector("#registerBtn");
 const showEvents = document.querySelector("#showEventsBtn");
 const backBtn = document.querySelector(".backBtn");
+const searchInput = document.querySelector("#searchInput");
+const searchBtn = document.querySelector("#searchBtn");
 
 // EVENTOS
 
@@ -31,65 +33,151 @@ registerBtn.addEventListener("click", (e) => {
   const nomeInput = document.querySelector("#name");
   const dataInput = document.querySelector("#data");
   const localInput = document.querySelector("#local");
-  const descriptionInput = document.querySelector("#description");
   const participantsInput = document.querySelector("#participants");
   const emailInput = document.querySelector("#email");
+  const imageInput = document.querySelector("#image");
 
   // coletando os valores dos inputs
   const nome = nomeInput.value;
   const data = dataInput.value;
   const local = localInput.value;
-  const description = descriptionInput.value;
   const participants = participantsInput.value;
   const email = emailInput.value;
+  const imageFile = imageInput.files[0];
 
-  if (
-    nome === "" ||
-    data === "" ||
-    local === "" ||
-    description === "" ||
-    participants === "" ||
-    email === ""
-  ) {
-    alert("Preencha todos os campos");
-    return;
+  //Manipulacao String
+  function normalizarEmail(email) {
+    return email.trim().toLowerCase();
   }
 
-  cadastrarEvento(nome, data, local, description, participants, email);
-  listarEventos();  
+  const emailNormalizado = normalizarEmail(email);
+  //
 
-  // limpando os inputs
-  nomeInput.value = "";
-  dataInput.value = "";
-  localInput.value = "";
-  descriptionInput.value = "";
-  participantsInput.value = "";
-  emailInput.value = "";
+  const nomeRegex = /^[A-Za-zÀ-ÿ\s]+$/;
+  const participantsRegex = /^\d+$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Regex (Expressoes Regulares)
+  if (!nome.match(nomeRegex)) {
+    alert("O nome deve conter apenas letras e espaços.");
+    return;
+  }
+  if (!local) {
+    alert("O local não pode estar vazio.");
+    return;
+  }
+  if (!participants.match(participantsRegex) || Number(participants) <= 0) {
+    alert("O número de participantes deve ser um valor inteiro positivo.");
+    return;
+  }
+  if (!emailNormalizado.match(emailRegex)) {
+    alert("Digite um e-mail válido.");
+    return;
+  }
+  if (!imageFile) {
+    alert("Selecione uma imagem para o evento.");
+    return;
+  }
+  //
+
+  const reader = new FileReader();
+  reader.onload = function () {
+    const image = reader.result;
+
+    cadastrarEvento(nome, data, local, participants, email, image);
+    listarEventos();
+
+    // limpando os inputs
+    nomeInput.value = "";
+    dataInput.value = "";
+    localInput.value = "";
+    participantsInput.value = "";
+    emailInput.value = "";
+    imageInput.value = "";
+  };
+
+  registerSucess();
+
+  setTimeout(() => {
+    alert("Evento cadastrado com sucesso!");
+  }, 1000);
+
+  reader.readAsDataURL(imageFile);
+});
+
+searchBtn.addEventListener("click", () => {
+  const query = searchInput.value.trim().toLowerCase();
+  filtrarEventos(query);
 });
 
 // FUNCOES
 
-function cadastrarEvento(nome, data, local, description, participants, email) {
+function filtrarEventos(query) {
+  const listaEventosUl = document.querySelector("#listaEventosUl");
+  listaEventosUl.innerHTML = "";
+
+  let eventos = JSON.parse(localStorage.getItem("eventos") || []);
+  const eventosFiltrados = eventos.filter((evento) => {
+    return evento.nome.toLowerCase().includes(query);
+  });
+
+  if (eventosFiltrados.length === 0) {
+    listaEventosUl.innerHTML = "Nenhum evento encontrado";
+  } else {
+    eventosFiltrados.forEach((evento) => {
+      const novoEventoLi = document.createElement("li");
+      novoEventoLi.innerHTML = `
+      <h3>${evento.nome}</h3>
+      <p>${evento.data}</p>
+      <p>${evento.local}</p>
+      <p>${evento.participants} pessoas</p>
+      <p>${evento.email}</p>
+      <img src="${evento.image}" alt="Imagem do Evento" style="max-width: 200px; height: auto; margin: 10px 0;" />
+      <button class="deleteBtn">Deletar</button>  
+    `;
+      listaEventosUl.appendChild(novoEventoLi);
+      const deleteBtn = novoEventoLi.querySelector(".deleteBtn");
+      deleteBtn.addEventListener("click", () => {
+        excluirEvento(evento);
+        novoEventoLi.remove();
+      });
+    });
+  }
+}
+
+function registerSucess() {
+  const labels = document.querySelectorAll("label");
+
+  labels.forEach((label) => {
+    label.style.color = "green";
+  });
+
+  setTimeout(() => {
+    labels.forEach((label) => {
+      label.style.color = "";
+    });
+  }, 1000);
+}
+
+function cadastrarEvento(nome, data, local, participants, email, image) {
   let novoEvento = {
     nome: nome,
     data: data,
     local: local,
-    description: description,
     participants: participants,
     email: email,
+    image: image,
   };
 
   let eventos = JSON.parse(localStorage.getItem("eventos")) || [];
   eventos.push(novoEvento);
   localStorage.setItem("eventos", JSON.stringify(eventos));
-
-
 }
 
 function listarEventos() {
   const listaEventosUl = document.querySelector("#listaEventosUl");
   listaEventosUl.innerHTML = "";
-  
+
   let eventos = JSON.parse(localStorage.getItem("eventos")) || [];
   eventos.forEach((evento) => {
     const novoEventoLi = document.createElement("li");
@@ -97,31 +185,25 @@ function listarEventos() {
     <h3>${evento.nome}</h3>
     <p>${evento.data}</p>
     <p>${evento.local}</p>
-    <p class="descriptionText">${evento.description}</p>
     <p>${evento.participants} pessoas</p>
     <p>${evento.email}</p>
+    <img src="${evento.image}" alt="Imagem do Evento" style="max-width: 200px; height: auto; margin: 10px 0;" />
     <button class="deleteBtn">Deletar</button>
     `;
     listaEventosUl.appendChild(novoEventoLi);
 
-
-    const deleteBtn = document.querySelector('.deleteBtn')
-    deleteBtn.addEventListener('click', () => {
-      excluirEvento(evento)
-      novoEventoLi.remove()
-  })
-})}
+    const deleteBtn = novoEventoLi.querySelector(".deleteBtn");
+    deleteBtn.addEventListener("click", () => {
+      excluirEvento(evento);
+      novoEventoLi.remove();
+    });
+  });
+}
 
 function excluirEvento(eventoParaExcluir) {
   let eventos = JSON.parse(localStorage.getItem("eventos")) || [];
-  eventos = eventos.filter(evento => evento.nome !== eventoParaExcluir.nome && evento.data !== eventoParaExcluir.data)
-  localStorage.setItem("eventos", JSON.stringify(eventos))
+  eventos = eventos.filter((evento) => evento.nome !== eventoParaExcluir.nome);
+  localStorage.setItem("eventos", JSON.stringify(eventos));
 }
 
-document.addEventListener("DOMContentLoaded", listarEventos)
-
-// O que falta
-
-// - Armazenar os dados no localStorage
-// - Criar uma função para listar os eventos do localStorage
-// - Criar uma função para deletar um evento do localStorage
+document.addEventListener("DOMContentLoaded", listarEventos);
